650eee6b78bfd2394a34503e10ad6111d60cfef6
sanya-shch <shchipkov.sanya@ukr.net>
2022-08-27 17:04:08 +0300

    added not found page

--- src/screens/NotFoundPage/index.js
+++ src/screens/NotFoundPage/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./style.scss";
 
 function NotFoundPage() {
-  return <div></div>;
+  // const randIndex = Math.floor(Math.random() * 3);
+
+  useEffect(() => {
+    const text1 = document.getElementById("text_1");
+    let shadow = "";
+
+    for (let i = 0; i < 30; i++) {
+      shadow += (shadow ? "," : "") + -i * 1 + "px " + i * 1 + "px 0 #d9d9d9";
+    }
+    text1.style.textShadow = shadow;
+  }, []);
+
+  return (
+    <div className="not_found_page">
+      <div id="text_1" data-text="404 Not Found">
+        404 Not Found
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
