df417baf0246131640c901d389159a789b111b74
sanya-shch <shchipkov.sanya@ukr.net>
2022-08-27 11:40:25 +0300

    added cardTypeIcons

---
+++ src/assets/cardTypeIcons/index.js
@@ -0,0 +1,29 @@
+import clover from "./clover.png";
+import diamond from "./diamond.png";
+import heart from "./heart.png";
+import spades from "./spades.png";
+
+import cloverGray from "./clover_gray.png";
+import diamondGray from "./diamond_gray.png";
+import heartGray from "./heart_gray.png";
+import spadesGray from "./spades_gray.png";
+
+import cloverBig from "./clover_big.png";
+import diamondBig from "./diamond_big.png";
+import heartBig from "./heart_big.png";
+import spadesBig from "./spades_big.png";
+
+export {
+  clover,
+  diamond,
+  heart,
+  spades,
+  cloverGray,
+  diamondGray,
+  heartGray,
+  spadesGray,
+  cloverBig,
+  diamondBig,
+  heartBig,
+  spadesBig,
+};
